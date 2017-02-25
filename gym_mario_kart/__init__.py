c76a285cee38b75597d576a0650b422839eaadf0
Brian Zier <zier.brian@gmail.com>
2017-02-25 13:09:52 -0800

    Further refactoring: support each track as an environment.

--- gym_mario_kart/__init__.py
+++ gym_mario_kart/__init__.py
@@ -3,14 +3,183 @@ from gym.envs.registration import register
 
 logger = logging.getLogger(__name__)
 
+
+##### MUSHROOM CUP ####
 register(
-    id='Mario-Kart-v0',
-    entry_point='gym_mario_kart.envs:MarioKartEnv',
-#    max_episode_steps=100000,
-    timestep_limit=100000,
+    id='Mario-Kart-Luigi-Raceway-v0',
+    entry_point='gym_mario_kart.envs:MarioKartLuigiRacewayEnv',
     tags={
         'mupen': True,
+        'cup': 'Mushroom',
         'wrapper_config.TimeLimit.max_episode_steps': 100000,
     },
-    nondeterministic = True,
+    nondeterministic=True,
+)
+
+register(
+    id='Mario-Kart-Moo-Moo-Farm-v0',
+    entry_point='gym_mario_kart.envs:MarioKartMooMooFarmEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Mushroom',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
+)
+
+register(
+    id='Mario-Kart-Koopa-Troopa-Beach-v0',
+    entry_point='gym_mario_kart.envs:MarioKartKoopaTroopaBeachEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Mushroom',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
+)
+
+register(
+    id='Mario-Kart-Kalimari-Desert-v0',
+    entry_point='gym_mario_kart.envs:MarioKartKalimariDesertEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Mushroom',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
+)
+
+##### FLOWER CUP ####
+register(
+    id='Mario-Kart-Toads-Turnpike-v0',
+    entry_point='gym_mario_kart.envs:MarioKartToadsTurnpikeEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Flower',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
+)
+
+register(
+    id='Mario-Kart-Frappe-Snowland-v0',
+    entry_point='gym_mario_kart.envs:MarioKartFrappeSnowlandEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Flower',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
+)
+
+register(
+    id='Mario-Kart-Choco-Mountain-v0',
+    entry_point='gym_mario_kart.envs:MarioKartChocoMountainEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Flower',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
+)
+
+register(
+    id='Mario-Kart-Mario-Raceway-v0',
+    entry_point='gym_mario_kart.envs:MarioKartMarioRacewayEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Flower',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
+)
+
+##### STAR CUP ####
+register(
+    id='Mario-Kart-Wario-Stadium-v0',
+    entry_point='gym_mario_kart.envs:MarioKartWarioStadiumEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Star',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
+)
+
+register(
+    id='Mario-Kart-Sherbet-Land-v0',
+    entry_point='gym_mario_kart.envs:MarioKartSherbetLandEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Star',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
+)
+
+register(
+    id='Mario-Kart-Royal-Raceway-v0',
+    entry_point='gym_mario_kart.envs:MarioKartRoyalRacewayEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Star',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
+)
+
+register(
+    id='Mario-Kart-Bowsers-Castle-v0',
+    entry_point='gym_mario_kart.envs:MarioKartBowsersCastleEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Star',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
+)
+
+##### SPECIAL CUP ####
+register(
+    id='Mario-Kart-DKs-Jungle-Parkway-v0',
+    entry_point='gym_mario_kart.envs:MarioKartDKsJungleParkwayEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Special',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
+)
+
+register(
+    id='Mario-Kart-Yoshi-Valley-v0',
+    entry_point='gym_mario_kart.envs:MarioKartYoshiValleyEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Special',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
+)
+
+register(
+    id='Mario-Kart-BansheeBoardwalk-v0',
+    entry_point='gym_mario_kart.envs:MarioKartBansheeBoardwalkEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Special',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
+)
+
+register(
+    id='Mario-Kart-Rainbow-Road-v0',
+    entry_point='gym_mario_kart.envs:MarioKartRainbowRoadEnv',
+    tags={
+        'mupen': True,
+        'cup': 'Special',
+        'wrapper_config.TimeLimit.max_episode_steps': 100000,
+    },
+    nondeterministic=True,
 )
--- gym_mario_kart/envs/mario_kart_env.py
+++ gym_mario_kart/envs/mario_kart_env.py
@@ -1,26 +1,10 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-
-import array
-import os
-import signal
-import subprocess
-import threading
-import time
-import pygame
+import abc
 from termcolor import cprint
-
-import gym
-from gym import error, spaces, utils
-from gym.utils import seeding
 from gym_mario_kart.envs.mupen64plus_env import Mupen64PlusEnv, Config, INTERNAL_STATE, IMAGE_HELPER
 
-import numpy as np
-
-import wx
-wx.App()
-
 ###############################################
-class MarioKartConfig:
+class MarioKartEnv(Mupen64PlusEnv):
+    __metaclass__ = abc.ABCMeta
 
     DEFAULT_STEP_REWARD = -1
     END_DETECTION_REWARD_REFUND = 215
@@ -29,28 +13,24 @@ class MarioKartConfig:
 
     BLACK_PIXEL = (0, 0, 0)
 
-    PLAYER_ROW = 1
-    PLAYER_COL = 1
+    PLAYER_ROW = 0
+    PLAYER_COL = 0
 
     MAP_SERIES = 0
     MAP_CHOICE = 0
 
-
-###############################################
-class MarioKartEnv(Mupen64PlusEnv):
-    metadata = {'render.modes': ['human']}
-
-    def __init__(self):
+    def __init__(self, character='mario'):
         super(MarioKartEnv, self).__init__('/home/brian/Programming/TensorKart/marioKart.n64')
-        
+        self._set_character(character)
+
     def _get_reward(self, episode_over):
         #cprint('Get Reward called!','red')
         if episode_over:
-            # Refund the reward lost in the frames following the race finish until end episode detection
-            return MarioKartConfig.END_DETECTION_REWARD_REFUND
+            # Refund the reward lost in the frames between the race finish and end episode detection
+            return self.END_DETECTION_REWARD_REFUND
         else:
             # Currently just -1 per step
-            return MarioKartConfig.DEFAULT_STEP_REWARD
+            return self.DEFAULT_STEP_REWARD
 
     def _evaluate_end_state(self):
         #cprint('Evaluate End State called!','red')
@@ -61,12 +41,12 @@ class MarioKartEnv(Mupen64PlusEnv):
         bottom_left = IMAGE_HELPER.GetPixelColor(pix_arr, 19, 460)
         bottom_right = IMAGE_HELPER.GetPixelColor(pix_arr, 620, 460)
 
-        if upper_left == upper_right == bottom_left == bottom_right == MarioKartConfig.BLACK_PIXEL:
+        if upper_left == upper_right == bottom_left == bottom_right == self.BLACK_PIXEL:
             INTERNAL_STATE.end_episode_confidence += 1
         else:
             INTERNAL_STATE.end_episode_confidence = 0
 
-        if INTERNAL_STATE.end_episode_confidence > MarioKartConfig.END_EPISODE_THRESHOLD:
+        if INTERNAL_STATE.end_episode_confidence > self.END_EPISODE_THRESHOLD:
             INTERNAL_STATE.is_end_episode = True
 
         return INTERNAL_STATE.is_end_episode
@@ -99,15 +79,15 @@ class MarioKartEnv(Mupen64PlusEnv):
 
             # Frame 150 is the 'Player Select' screen
             if frame == 150:
-                print('Player row: ', str(MarioKartConfig.PLAYER_ROW))
-                print('Player col: ', str(MarioKartConfig.PLAYER_COL))
+                print('Player row: ', str(self.PLAYER_ROW))
+                print('Player col: ', str(self.PLAYER_COL))
 
-                if cur_row != MarioKartConfig.PLAYER_ROW:
+                if cur_row != self.PLAYER_ROW:
                     action = Config.JOYSTICK_DOWN
                     cur_row += 1
 
             if frame in range(151, 156) and frame % 2 == 0:
-                if cur_col != MarioKartConfig.PLAYER_COL:
+                if cur_col != self.PLAYER_COL:
                     action = Config.JOYSTICK_RIGHT
                     cur_col += 1
 
@@ -115,16 +95,16 @@ class MarioKartEnv(Mupen64PlusEnv):
             if frame == 195:
                 cur_row = 0
                 cur_col = 0
-                print('Map series: ', str(MarioKartConfig.MAP_SERIES))
-                print('Map choice: ', str(MarioKartConfig.MAP_CHOICE))
+                print('Map series: ', str(self.MAP_SERIES))
+                print('Map choice: ', str(self.MAP_CHOICE))
 
             if frame in range(195, 202) and frame %2 == 0:
-                if cur_col != MarioKartConfig.MAP_SERIES:
+                if cur_col != self.MAP_SERIES:
                     action = Config.JOYSTICK_RIGHT
                     cur_col += 1
 
             if frame in range(223, 230) and frame %2 == 0:
-                if cur_row != MarioKartConfig.MAP_CHOICE:
+                if cur_row != self.MAP_CHOICE:
                     action = Config.JOYSTICK_DOWN
                     cur_row += 1
 
@@ -133,3 +113,15 @@ class MarioKartEnv(Mupen64PlusEnv):
 
             self._take_action(action)
             frame += 1
+
+    def _set_character(self, character):
+        characters = {'mario'  : (0,0),
+                      'luigi'  : (0,1),
+                      'peach'  : (0,2),
+                      'toad'   : (0,3),
+                      'yoshi'  : (1,0),
+                      'd.k.'   : (1,1),
+                      'wario'  : (1,2),
+                      'bowser' : (1,3)}
+
+        self.PLAYER_ROW, self.PLAYER_COLUMN = characters[character]
