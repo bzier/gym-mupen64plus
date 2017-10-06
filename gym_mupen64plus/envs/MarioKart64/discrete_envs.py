ffaf40ffab93e4f51c48f785ed9ca6476078c3f1
Brian Zier <zier.brian@gmail.com>
2017-10-05 19:19:39 -0700

    Add initial discrete action space environment

---
+++ gym_mupen64plus/envs/MarioKart64/discrete_envs.py
@@ -0,0 +1,33 @@
+import abc
+from gym_mupen64plus.envs.MarioKart64.track_envs import MarioKartLuigiRacewayEnv
+from gym import spaces
+
+class MarioKartDiscreteLuigiRacewayEnv(MarioKartLuigiRacewayEnv):
+
+    ACTION_MAP = [
+        ("NO_OP",         [  0,   0, 0, 0, 0]),
+        ("STRAIGHT",      [  0,   0, 1, 0, 0]),
+        ("BRAKE",         [  0,   0, 0, 1, 0]),
+        ("BACK_UP",       [  0, -80, 0, 1, 0]),
+        ("SOFT_LEFT",     [-20,   0, 1, 0, 0]),
+        ("LEFT",          [-40,   0, 1, 0, 0]),
+        ("HARD_LEFT",     [-60,   0, 1, 0, 0]),
+        ("EXTREME_LEFT",  [-80,   0, 1, 0, 0]),
+        ("SOFT_RIGHT",    [ 20,   0, 1, 0, 0]),
+        ("RIGHT",         [ 40,   0, 1, 0, 0]),
+        ("HARD_RIGHT",    [ 60,   0, 1, 0, 0]),
+        ("EXTREME_RIGHT", [ 80,   0, 1, 0, 0]),
+    ]
+
+    def __init__(self):
+        super(MarioKartDiscreteLuigiRacewayEnv, self).__init__()
+
+        self.action_space = spaces.Discrete(12)
+
+    
+    def _step(self, action):
+        print 'Step called with: ', action
+        controls = self.ACTION_MAP[action][1]
+        print 'Using controls: ', controls
+
+        return super(MarioKartDiscreteLuigiRacewayEnv, self)._step(controls)
