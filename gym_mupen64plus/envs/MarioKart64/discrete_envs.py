e1b56e11c0768a9be514df32e60694a7c29d7c0c
Brian Zier <zier.brian@gmail.com>
2017-12-02 15:20:53 -0800

    Simple discrete env refactor

--- gym_mupen64plus/envs/MarioKart64/discrete_envs.py
+++ gym_mupen64plus/envs/MarioKart64/discrete_envs.py
@@ -2,8 +2,7 @@ import abc
 from gym_mupen64plus.envs.MarioKart64.track_envs import MarioKartLuigiRacewayEnv
 from gym import spaces
 
-class MarioKartDiscreteLuigiRacewayEnv(MarioKartLuigiRacewayEnv):
-
+class DiscreteActions:
     ACTION_MAP = [
         ("NO_OP",         [  0,   0, 0, 0, 0]),
         ("STRAIGHT",      [  0,   0, 1, 0, 0]),
@@ -19,14 +18,27 @@ class MarioKartDiscreteLuigiRacewayEnv(MarioKartLuigiRacewayEnv):
         ("EXTREME_RIGHT", [ 80,   0, 1, 0, 0]),
     ]
 
+    @staticmethod
+    def get_action_space():
+        return spaces.Discrete(len(DiscreteActions.ACTION_MAP))
+
+    @staticmethod
+    def get_controls_from_action(action):
+        return DiscreteActions.ACTION_MAP[action][1]
+
+
+class MarioKartDiscreteLuigiRacewayEnv(MarioKartLuigiRacewayEnv):
+
     ENABLE_CHECKPOINTS = True
 
     def __init__(self):
         super(MarioKartDiscreteLuigiRacewayEnv, self).__init__()
 
-        self.action_space = spaces.Discrete(12)
+        # This needs to happen after the parent class init so we can effectively override the action space
+        self.action_space = DiscreteActions.get_action_space()
 
     def _step(self, action):
-        controls = self.ACTION_MAP[action][1]
+        # Interpret the action choice and get the actual controller state for this step
+        controls = DiscreteActions.get_controls_from_action(action)
 
         return super(MarioKartDiscreteLuigiRacewayEnv, self)._step(controls)
