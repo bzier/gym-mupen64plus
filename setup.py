a4750be06461a2cbfa3128e338dfa8e901983a37
Brian Zier <zier.brian@gmail.com>
2018-03-24 11:36:23 -0700

    Lock all dependency versions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='gym_mupen64plus',
       version='0.0.2',
       install_requires=['gym==0.7.4',
-                        'numpy>=1.12.0',
-                        'PyYAML>=3.12',
-                        'termcolor>=1.1.0',
-                        'mss>=3.0.1'])
+                        'numpy==1.14.2',
+                        'PyYAML==3.12',
+                        'termcolor==1.1.0',
+                        'mss==3.2.0'])
