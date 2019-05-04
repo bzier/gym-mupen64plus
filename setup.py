4c3064f2e843f9862beda2323582ddbf574f29f8
Brian Zier <zier.brian@gmail.com>
2019-05-04 15:23:38 -0700

    Upgrading dependencies
    Mostly upgrading to get rid of the Github warning for PyYAML (CVE-2017-18342), but figured I might as well upgrade a few others as well... Gym still needs to be upgraded (see #41).
    I did some really basic testing to ensure stuff still runs. No guarantees that I didn't break something subtle though...

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='gym_mupen64plus',
-      version='0.0.2',
+      version='0.0.3',
       install_requires=['gym==0.7.4',
-                        'numpy==1.14.2',
-                        'PyYAML==3.12',
+                        'numpy==1.16.2',
+                        'PyYAML==5.1',
                         'termcolor==1.1.0',
-                        'mss==3.2.0',
-                        'opencv-python==3.4.1.15'])
+                        'mss==4.0.2', # 4.0.3 removes support for Python 2.7
+                        'opencv-python==4.1.0.25'])
