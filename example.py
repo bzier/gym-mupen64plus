42385f504f5362a510b793bfa48614fc1dfc1041
Enric Moreu <enric.moreu.filella@gmail.com>
2019-09-26 01:27:41 +0100

    Start VNC in parallel

---
+++ example.py
@@ -0,0 +1,21 @@
+#!/bin/python
+import gym, gym_mupen64plus
+import subprocess
+
+def startVNC(display):
+    subprocess.call("x11vnc -display :{} -localhost -forever -viewonly &".format(str(display)), shell=True)
+
+env = gym.make('Mario-Kart-Luigi-Raceway-v0')
+env.reset()
+
+startVNC(1)
+
+for i in range(88):
+    (obs, rew, end, info) = env.step([0, 0, 0, 0, 0]) # NOOP until green light
+
+for i in range(10000):
+    (obs, rew, end, info) = env.step([0, 0, 1, 0, 0]) # Drive straight
+
+raw_input("Press <enter> to exit... ")
+
+env.close()
