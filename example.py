b72550659d00a9d0b409c2c6a5e407ab2ede820c
Brian Zier <zier.brian@gmail.com>
2021-01-30 15:48:53 -0800

    Exclude ROMs from image and bind mount

--- example.py
+++ example.py
@@ -4,13 +4,18 @@ import gym, gym_mupen64plus
 env = gym.make('Mario-Kart-Luigi-Raceway-v0')
 env.reset()
 
+print("NOOP waiting for green light")
 for i in range(18):
     (obs, rew, end, info) = env.step([0, 0, 0, 0, 0]) # NOOP until green light
 
+print("GO! ...drive straight as fast as possible...")
 for i in range(50):
     (obs, rew, end, info) = env.step([0, 0, 1, 0, 0]) # Drive straight
 
-for i in range(10000):
+print("Doughnuts!!")
+for i in range(100):
+    if i % 100 == 0:
+        print("Step " + str(i))
     (obs, rew, end, info) = env.step([-80, 0, 1, 0, 0]) # Hard-left doughnuts!
     (obs, rew, end, info) = env.step([-80, 0, 0, 0, 0]) # Hard-left doughnuts!
 
